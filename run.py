feac75e76148669ce30ef11ec2daca512f94da1a
Nguyen Tran <me@mac.local>
2025-09-18 20:18:23 -0500

    add absolute path

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 from maia2 import train, utils
-cfg = utils.parse_args(cfg_file_path="./maia2_models/config.yaml")
+cfg = utils.parse_args(cfg_file_path="/kaggle/working/maia2/maia2_models/config.yaml")
 train.run(cfg)
